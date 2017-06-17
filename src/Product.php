9cef809c837a5ab92e5de75786aea74ed29ae853
Кирилл Левунин <klevunin@gmail.com>
2017-06-17 13:36:42 +0300

    up

--- src/Product.php
+++ src/Product.php
@@ -12,8 +12,9 @@ class Product
      * @param $data
      * @methods string: Create,Delete
      * @store_id id store api http://developer.mailchimp.com/documentation/mailchimp/reference/ecommerce/stores/
+     * @api_key_mailchimp api key mailchimp
      */
-    function __construct($data, $methods = 'Create',$store_id)
+    function __construct($data, $methods = 'Create',$store_id,$api_key_mailchimp)
     {
         if (!isset($data['id'])) {
             return 'ERROR: No DATA ID product http://developer.mailchimp.com/documentation/mailchimp/reference/ecommerce/stores/products/';
@@ -24,6 +25,8 @@ class Product
         if (!isset($data['variants'])) {
             return 'ERROR: No variants product http://developer.mailchimp.com/documentation/mailchimp/reference/ecommerce/stores/products/';
         }
+
+        $MailChimp = new MailChimp($api_key_mailchimp);
 
         if ($methods == 'Create') {
             //проверим возможно продукт уже существует
