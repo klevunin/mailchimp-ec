6c1af306ab0a94a331fe0d90fb03e7121d3328cb
Кирилл Левунин <klevunin@gmail.com>
2017-06-17 14:17:48 +0300

    up

--- src/Product.php
+++ src/Product.php
@@ -7,6 +7,11 @@ use \DrewM\MailChimp\MailChimp;
 class Product
 {
 
+    private $data = array();
+    private $methods = 'Read';
+    private $store_id;
+    private $api_key_mailchimp;
+
     /**
      * Product constructor.
      * @param $data
@@ -14,7 +19,7 @@ class Product
      * @store_id id store api http://developer.mailchimp.com/documentation/mailchimp/reference/ecommerce/stores/
      * @api_key_mailchimp api key mailchimp
      */
-    function __construct($data, $methods = 'Create',$store_id,$api_key_mailchimp)
+    function __construct($data, $methods = 'Reade',$store_id,$api_key_mailchimp)
     {
         if (!isset($data['id'])) {
             return 'ERROR: No DATA ID product http://developer.mailchimp.com/documentation/mailchimp/reference/ecommerce/stores/products/';
@@ -26,19 +31,72 @@ class Product
             return 'ERROR: No variants product http://developer.mailchimp.com/documentation/mailchimp/reference/ecommerce/stores/products/';
         }
 
+        if (is_null($methods)) {
+            $methods ='Create';
+        }
+
+
         $MailChimp = new MailChimp($api_key_mailchimp);
 
-        if ($methods == 'Create') {
+        $this->data=$data;
+        $this->methods=$methods;
+        $this->store_id=$store_id;
+        $this->api_key_mailchimp=$api_key_mailchimp;
+
+        if ($methods == 'Read') {
             //проверим возможно продукт уже существует
-            $result = $MailChimp->get("/ecommerce/stores/" . $store_id . "/products/testdf", [
-            ]);
+            $result = $this->readProduct($MailChimp);
+
+            if ($result['status'] == 404) {
+                //товар нет или ошибка. Пробую создать товар
+                print '<pre>';
+                print_r($result);
+            } else {
+                //товар есть обновим товар
+                $result = editProduct($MailChimp);
+                print '<pre>';
+                print_r($result);
+            }
+
+
 
         } else {
-            print '!'.$methods;
+            if ($methods == 'Create') {
+                $result = createProduct($MailChimp);
+            } elseif ($methods == 'Edit') {
+                $result = editProduct($MailChimp);
+            } elseif ($methods == 'Delete') {
+                $result = deleteProduct($MailChimp);
+            }
+
         }
 
 
 
     }
+
+    function readProduct($MailChimp){
+        $result = $MailChimp->get("/ecommerce/stores/" . $this->store_id . "/products/".$this->data['id'], [
+        ]);
+    }
+
+    function createProduct($MailChimp){
+        $result = $MailChimp->post("/ecommerce/stores/" . $this->store_id . "/products/".$this->data['id'], [
+            ".$this->data."
+        ]);
+    }
+
+    function editProduct($MailChimp){
+        $result = $MailChimp->patch("/ecommerce/stores/" . $this->store_id . "/products/".$this->data['id'], [
+            ".$this->data."
+        ]);
+
+    }
+
+    function deleteProduct($MailChimp){
+        $result = $MailChimp->delete("/ecommerce/stores/" . $this->store_id . "/products/".$this->data['id'], [
+        ]);
+    }
+
 
 }
